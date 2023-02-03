bd8fc62527ebe04cc192abb861c97485fc33c1f9
Leon <leon.van.den.heuvel@hva.nl>
2023-02-03 23:55:16 +0100

    added playercontroller

--- Plant Theft Mania/Assets/Scripts/PlayerController.cs
+++ Plant Theft Mania/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public CharacterController2D controller;
+    public float runSpeed = 40f;
+
+    private float horizontalMove = 0f;
+    private bool jump = false;
+    private bool crouch = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        } else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+    }
+    // FixedUpdate is called once per fixed frame (100 fps)
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
     }
 }
